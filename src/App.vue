44b6905be72f8b64ac4e9846d5a35f9e543354b6
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-10-26 15:25:59 -0400

    Update `README.md` with Bulma & Buefy usage details; enhance `App.vue` layout and examples

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-  <b-button type="is-primary">Click me</b-button>
-  <b-button icon-left="heart">Profile</b-button>
-  <b-button icon-left="user">Profile</b-button>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-half">
+          <h1 class="title">Hello World</h1>
+          <p class="subtitle">Using Bulma classes</p>
+        </div>
+        <div class="column is-half">
+          <b-button type="is-primary">Click me</b-button>
+          <b-button icon-left="heart">Profile</b-button>
+          <b-button icon-left="user">Profile</b-button>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
