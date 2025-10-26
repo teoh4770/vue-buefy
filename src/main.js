3e107039d96b96640899e1deafadd243d19e870d
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-10-26 15:06:28 -0400

    Integrate Buefy plugin and styles in `main.js` after project setup; Also include sample for integrating individual components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import Buefy Full Bundle
+import Buefy from 'buefy'
+import 'buefy/dist/css/buefy.css'
+
+// Or individual components as Vue plugins
+// import { Table, Input } from 'buefy'
+// import 'buefy/dist/css/buefy.css'
+
+const app = createApp(App)
+
+// Install plugins here
+// Register Buefy plugin
+app.use(Buefy);
+
+// Register Table and Input Buefy component
+// app.use(Table)
+// app.use(Input)
+
+app.mount('#app')
